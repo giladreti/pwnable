991bad48926e60b1f80fa3b2268f08114a30aca6
Gilad Reti <gilad.reti@gmail.com>
2025-07-22 00:18:06 +0300

    adapt towelroot to 2025

--- towelroot/run.sh
+++ towelroot/run.sh
@@ -1,2 +1,16 @@
-#!/bin/sh
-qemu-system-arm -M vexpress-a9 -m 110M -kernel zImage -initrd ramdisk.img -append 'root=/dev/ram rw console=ttyAMA0 rdinit=/sbin/init' -nographic -monitor /dev/null
+#!/bin/bash
+sudo docker stop towelroot_chall
+sudo docker rm towelroot_chall
+./build.sh
+docker run -d \
+  --name towelroot_chall \
+  --user 1138:1138 \
+  --cap-drop ALL \
+  --security-opt no-new-privileges \
+  --read-only \
+  --tmpfs /tmp:rw,size=64m \
+  --pids-limit 100 \
+  --memory=128m \
+  --cpus=1.0 \
+  -p 9902:9902 \
+  towelroot
